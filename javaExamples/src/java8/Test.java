c864df77ed258b171b06233bf1cd745922bf30d7
vishalm <sunnymahajan89@gmail.com>
2014-09-25 12:02:58 +0100

    java 8 code

---
+++ javaExamples/src/java8/Test.java
@@ -0,0 +1,19 @@
+package java8;
+
+import java8.functionalinterface.Service;
+
+public class Test {
+	public static void main(String[] args) {
+		Service aa;
+		aa = (f,j) -> {
+			int k = f * j;
+			return k;
+		};
+		System.out.println(aa.getValue(10,5));
+		System.out.println(aa.getName());
+
+		aa = (i,j) -> i + j;
+		System.out.println(aa.getValue(10,5));
+
+	}
+}
